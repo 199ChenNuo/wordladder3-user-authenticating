a7bcebbbc214d89711b0f6452e972fe46ce2cf48
199ChenNuo <cn.222@sjtu.edu.cn>
2018-04-29 14:03:06 +0800

    spring security

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,16 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.*;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
--- src/main/java/com/example/demo/WordLadderController.java
+++ src/main/java/com/example/demo/WordLadderController.java
@@ -1,23 +1,18 @@
-package com.example.demo.controller;
-
+package com.example.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.WordLadder;
-//import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
 import java.util.Set;
-
-@RestController
-public class UserController {
+public class WordLadderController {
     WordLadder wl = new WordLadder();
 
-    protected static Logger logger=LoggerFactory.getLogger(UserController.class);
+    protected static Logger logger=LoggerFactory.getLogger(WordLadderController.class);
 
-    @GetMapping("/wordladder/{name}")
-    @RequestMapping(value="/wordladder/{name}", method = RequestMethod.GET)
+    @GetMapping("/wordladder")
+    @RequestMapping(value="/wordladder", method = RequestMethod.GET)
     public String helloworld(@RequestParam String w1, @RequestParam String w2) {
         logger.debug("输入w1,Name={}",w1);
         logger.debug("输入w2,Name={}",w2);
